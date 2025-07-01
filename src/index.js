592612c4ef6a99d5a837ec4ed9075c100771c44c
akhlashashmi <akhlasahmed.aka@gmail.com>
2025-07-01 13:21:36 +0500

    Utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import 'dotenv/config'
-import connectDB from "./db/index.js";
+import connectDB from "./db/index.js"
+import app from "./app.js";
 
 // const app = express()
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch(err => {
+    console.error('Database connection error:', err);
+    process.exit(1);
+});
 
 
 
---
+++ src/utils/apiError..js
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+    constructor(
+        statueCode,
+        message='Something went wrong',
+        errors = [],
+        stacks = ''
+    ) {
+        super(message)
+        this.statueCode = statueCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors = this.errors
+
+        if(stacks) {
+            this.stack = stacks
+        } else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export default ApiError 
